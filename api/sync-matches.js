6a17f7c67548f3f9f83ace56ebbccc391d14504c
Peymex <peyman.makhdoomi@gmail.com>
2026-05-22 23:51:45 -0400

    Create sync-matches.js

---
+++ api/sync-matches.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
+
+  try {
+    const response = await fetch('https://api.football-data.org/v4/competitions/2000/matches', {
+      headers: { 'X-Auth-Token': process.env.FOOTBALL_API_KEY }
+    })
+    const data = await response.json()
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
