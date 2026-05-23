220c09aa695ec76267f62c1e3a7629cb03bb0149
Peymex <peyman.makhdoomi@gmail.com>
2026-05-23 00:58:36 -0400

    Update sync-matches.js

--- api/sync-matches.js
+++ api/sync-matches.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
 
   try {
     const response = await fetch(
